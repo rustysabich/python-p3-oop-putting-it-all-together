8183d6ffd15c06286a3301e0442dee6472b150ef
evansochieng <evansochieng121@gmail.com>
2023-09-13 20:17:37 +0300

    complete lab on properties

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    
+    # define attributes
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+        
+    # define page_count as property
+    def get_page_count(self):
+        return self._page_count
+    
+    def set_page_count(self, page_count):
+        if type(page_count) == int:
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+       
+    # compile the getter and setter methods of page_count attribute 
+    page_count = property(get_page_count, set_page_count)
+    
+    ### methods ###
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    
+    # initialize attributes
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        
+    # define size attribute as a property
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, size):
+        if type(size) == int:
+            self._size = size
+        else:
+            print("size must be an integer")
+            
+    # compile the getter and setter methods
+    size = property(get_size, set_size)
+    
+    ### methods ###
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = 'New'
